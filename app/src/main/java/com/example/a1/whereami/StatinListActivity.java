fd7c7480f2289aec95d95a57551636b990b87e8e
her2814 <her2814@gmail.com>
2018-06-04 14:43:22 +0900

    url커넥션을 통한 xml얻어오기 성공

--- app/src/main/java/com/example/a1/whereami/StatinListActivity.java
+++ app/src/main/java/com/example/a1/whereami/StatinListActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class StatinListActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     StatinListAdapter adapter;
+    EditText stationNameEt;
+    Button search_button;
     ArrayList<Station> stations = new ArrayList<>();
 
     @Override
@@ -19,10 +25,24 @@ public class StatinListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bus_metro_station_list);
 
+        search_button = findViewById(R.id.searchbutton);
+        stationNameEt = findViewById(R.id.stationName);
         recyclerView = findViewById(R.id.stationList);
+
         setData();
         setRecyclerView();
         Log.e("스테이션",stations.get(0).getBusstopname());
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stname = stationNameEt.getText().toString();
+                StationParsing stationParsing = new StationParsing(stname);
+                stationParsing.execute();
+            }
+        });
+
+
     }
     void setRecyclerView(){
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
--- app/src/main/java/com/example/a1/whereami/StationParsing.java
+++ app/src/main/java/com/example/a1/whereami/StationParsing.java
@@ -1,6 +1,72 @@
 package com.example.a1.whereami;
 
-public class StationParsing {
-    String API_KEY = "nQXBhoqZKdnFHvwi2%2Bl6JZO4garNidtHzdktRpqhjVH9GX5saW9tv5HNeSLWrSDFbAf9iXRnVIWmWToD6n1xTA%3D%3D";
+import android.os.AsyncTask;
+import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class StationParsing extends AsyncTask<Void,Void,Void>{
+    String API_KEY = "nQXBhoqZKdnFHvwi2%2Bl6JZO4garNidtHzdktRpqhjVH9GX5saW9tv5HNeSLWrSDFbAf9iXRnVIWmWToD6n1xTA%3D%3D";
+    Station station;
+    String station_name;
+
+    StationParsing(String station_name){
+        this.station_name = station_name;
+    }
+
+    public void getResponse(){
+        String strurl = "http://61.43.246.153/openapi-data/service/busanBIMS2/busStop?bstopnm=" + station_name + "&serviceKey=nQXBhoqZKdnFHvwi2%2Bl6JZO4garNidtHzdktRpqhjVH9GX5saW9tv5HNeSLWrSDFbAf9iXRnVIWmWToD6n1xTA%3D%3D&numOfRows=100&pageNo=1";
+        try {
+            URL url = new URL(strurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+                StringBuffer requset_result = new StringBuffer();
+                String line;
+
+                while((line = br.readLine())!=null) {
+                    requset_result.append(line);
+                }
+
+                Log.e("urlresponse : ", String.valueOf(requset_result));
+            }
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        getResponse();
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
 }
